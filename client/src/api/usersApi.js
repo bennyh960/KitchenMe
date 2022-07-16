86b8c3770514a079f99c44d647b3b39abf212930
bennyh960 <bennyh960@gmail.com>
2022-07-17 01:34:16 +0300

    change url

--- client/src/api/usersApi.js
+++ client/src/api/usersApi.js
@@ -4,36 +4,46 @@ import axios from "axios";
 // const url = "http://localhost:5000";
 const url = "";
 const newUserRouter = axios.create({
-  baseURL: `${url}/users/register`,
+  // baseURL: `${url}/users/register`,
+  baseURL: `/users/register`,
 });
 const findUserRouter = axios.create({
-  baseURL: `${url}/users/login`,
+  // baseURL: `${url}/users/login`,
+  baseURL: `/users/login`,
 });
 const resetPassword = axios.create({
-  baseURL: `${url}/users/reset/password`,
+  // baseURL: `${url}/users/reset/password`,
+  baseURL: `/users/reset/password`,
 });
 const logoutRouter = axios.create({
-  baseURL: `${url}/users/logout`,
+  // baseURL: `${url}/users/logout`,
+  baseURL: `/users/logout`,
 });
 const users = axios.create({
-  baseURL: `${url}/users`,
+  // baseURL: `${url}/users`,
+  baseURL: `/users`,
 });
 const userUploadAvatar = axios.create({
-  baseURL: `${url}/users/me/avatar`,
+  baseURL: `/users/me/avatar`,
+  // baseURL: `${url}/users/me/avatar`,
 });
 const getOtherProfile = axios.create({
-  baseURL: `${url}/users/profile`, //ep : /:userId
+  // baseURL: `${url}/users/profile`, //ep : /:userId
+  baseURL: `/users/profile`, //ep : /:userId
 });
 const getUserLists = axios.create({
-  baseURL: `${url}/user/lists`,
+  // baseURL: `${url}/user/lists`,
+  baseURL: `/user/lists`,
 });
 // * send request and answere with different endpoints('/request','/accept')
 const friendshipRouter = axios.create({
-  baseURL: `${url}/users/friend`,
+  // baseURL: `${url}/users/friend`,
+  baseURL: `/users/friend`,
 });
 
 const userNotifications = axios.create({
-  baseURL: `${url}/user/notifications`,
+  // baseURL: `${url}/user/notifications`,
+  baseURL: `/user/notifications`,
 });
 
 const usersApi = {
