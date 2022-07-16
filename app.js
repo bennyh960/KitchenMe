a8235f3e4099ae03f48582c7d614cd42d8326645
bennyh960 <bennyh960@gmail.com>
2022-07-17 02:24:03 +0300

    v6

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const chalk = require("chalk");
 const cors = require("cors");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 const socket = require("socket.io");
-const path = require("path");
-dotenv.config();
+// dotenv.config();
 
 // my files import
 require("./server/src/db/mongoose");
@@ -22,7 +21,6 @@ const PORT = process.env.PORT || 5000;
 // app use
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, "../client.build")));
 app.use(cors());
 
 // app use router
@@ -34,9 +32,6 @@ app.use("/api/messages", messangerRouter);
 // * Step to connect heroku
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
 }
 
 const server = app.listen(PORT, () => {
