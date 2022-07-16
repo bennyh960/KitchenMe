d408ff0b0f87d0561fe9f87dcd5d579775ef2cb9
bennyh960 <bennyh960@gmail.com>
2022-07-17 02:57:19 +0300

    v10

--- client/src/api/recipes.users.Api.js
+++ client/src/api/recipes.users.Api.js
@@ -1,27 +1,31 @@
 import axios from "axios";
 
 // *dev url
-// const url = "http://localhost:5000";
-const url = "";
+const url = "http://localhost:5000";
+// const url = "";
 
 const createNewRecipe = axios.create({
-  baseURL: `${url}/recipes/new`,
+  // baseURL: `${url}/recipes/new`,
+  baseURL: `/recipes/new`,
   headers: {
     "content-type": "multipart/form-data", // do not forget this
     Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
   },
 });
 const getPublicRecipes = axios.create({
-  baseURL: `${url}/recipes/public`,
+  // baseURL: `${url}/recipes/public`,
+  baseURL: `/recipes/public`,
 });
 const getUserRecipes = axios.create({
-  baseURL: `${url}/recipes/user`,
+  // baseURL: `${url}/recipes/user`,
+  baseURL: `/recipes/user`,
   headers: {
     Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
   },
 });
 const getFriendsPostsRouter = axios.create({
-  baseURL: `${url}/recipes/friends`,
+  // baseURL: `${url}/recipes/friends`,
+  baseURL: `/recipes/friends`,
 });
 
 const recipiesAPI = {
